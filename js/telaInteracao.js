e32d98b693c3410af7c0452caefc4b69ffdbeded
bsouzaluan <luan.barbosa@sptech.school>
2022-11-22 23:22:54 -0300

    tela sobre mim alguns detalhes na tela de interaçao e alguns detalhes na tela principal

--- js/telaInteracao.js
+++ js/telaInteracao.js
@@ -1,141 +1,141 @@
-var myQuestions = [
+var minhasQuestoes = [
 	{
-		question: "1 - Em que ano foi lançado o primeiro oculos da linha X-metal ?",
-		answers: {
+		pergunta: "1 - Em que ano foi lançado o primeiro oculos da linha X-metal ?",
+		questao: {
 			a: '1998',
 			b: '1997',
 			c: '1995',
 			d: '1996',
 		},
-		correctAnswer: 'b'
+		respostaCorreta: 'b'
 	},
 	{
-		question: "2 - qual foi o teste que o dono da marca fez, para testar as resistências das lentes",
-		answers: {
+		pergunta: "2 - qual foi o teste que o dono da marca fez, para testar as resistências das lentes",
+		questao:  {
 			a: 'Facadas na lente',
 			b: 'Tiro na lente',
 			c: 'Jogar as lentes no chão',
 			d: 'Colocou as lentes em uma prensa'
 		},
-		correctAnswer: 'b'
+		respostaCorreta: 'b'
 	},
 	{
-		question: "3 - em que ano a marca foi criada ?",
-		answers: {
+		pergunta: "3 - em que ano a marca foi criada ?",
+		questao:  {
 			a: '1973',
 			b: '1978',
 			c: '1975',
 			d: '1976'
 		},
-		correctAnswer: 'c'
+		respostaCorreta: 'c'
 	},
 	{
-		question: "4 - Qual o nome do criador da marca ?",
-		answers: {
+		pergunta: "4 - Qual o nome do criador da marca ?",
+		questao:  {
 			a: 'Jim Jannard',
 			b: 'Bill Oakley',
 			c: 'Jimer Jannard',
 			d: 'Harry Jamerson'
 		},
-		correctAnswer: 'a'
+		respostaCorreta: 'a'
 	},
 	{
-		question: "5 - Qual foi a origem do nome da marca oakley ?",
-		answers: {
+		pergunta: "5 - Qual foi a origem do nome da marca oakley ?",
+		questao: {
 			a: 'Foi por causa do seu passaro',
 			b: 'Foi por causa de seu gato',
 			c: 'Foi por causa do seu cachorro',
 			d: 'Foi por causa de seu filho'
 		},
-		correctAnswer: 'c'
+		respostaCorreta: 'c'
 	},
 	{
-		question: "6 - Qual foi o primeiro produto começado a ser fabricado pela oakley ?",
-		answers: {
+		pergunta: "6 - Qual foi o primeiro produto começado a ser fabricado pela oakley ?",
+		questao:  {
 			a: 'Manoplas de motocross',
 			b: 'Borrachas para as pernas dos oculos',
 			c: 'Oculos de sol',
 			d: 'Luvas para proteção'
 		},
-		correctAnswer: 'a'
+		respostaCorreta:'a'
 	},
 	{
-		question: "7 - Os oculos da linha x-metal possui numero de identificação ?",
-		answers: {
+		pergunta: "7 - Os oculos da linha x-metal possui numero de identificação ?",
+		questao:  {
 			a: 'Sim',
 			b: 'Não',
 			
 		},
-		correctAnswer: 'a'
+		respostaCorreta: 'a'
 	},
 	{
-		question: "8 - Em que ano foi fabricado a ultima juliet ?",
-		answers: {
+		pergunta: "8 - Em que ano foi fabricado a ultima juliet ?",
+		questao:  {
 			a: '2007',
 			b: '2006',
 			c: '2008',
 			d: '2009'
 		},
-		correctAnswer: 'd'
+		respostaCorreta: 'd'
 	},
 	{
-		question: "9 - Qual foi o ultimo oculos fabricado pela linha x-metal ?",
-		answers: {
+		pergunta: "9 - Qual foi o ultimo oculos fabricado pela linha x-metal ?",
+		questao:  {
 			a: 'Juliet',
 			b: 'X-squared',
 			c: 'Romeu 2',
 			d: 'Mars'
 		},
-		correctAnswer: 'b'
+		respostaCorreta: 'b'
 	},
 	{
-		question: "10 - Em que ano foi descontinuada a linha x-metal ?",
-		answers: {
+		pergunta: "10 - Em que ano foi descontinuada a linha x-metal ?",
+		questao: {
 			a: '2011',
 			b: '2009',
 			c: '2012',
 			d: '2010'
 		},
-		correctAnswer: 'c'
+		respostaCorreta: 'c'
 	}
 ];
 
 var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
+var resultadoContainer = document.getElementById('resultado');
 var submitButton = document.getElementById('submit');
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+generateQuiz(minhasQuestoes, quizContainer, resultadoContainer, submitButton);
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+function generateQuiz(questoes, quizContainer, resultadoContainer, submitButton){
 
-	function showQuestions(questions, quizContainer){
+	function showQuestions(questoes, quizContainer){
 		// precisaremos de um lugar para armazenar a saída e as opções de resposta
 		var output = [];
-		var answers; // poderia ter dado outro nome, alem do mais, eu mesmo me confundi :/
+		var questao; // poderia ter dado outro nome, alem do mais, eu mesmo me confundi :/
 
 		// para cada questão
-		for(var i=0; i<questions.length; i++){
+		for(var i=0; i<questoes.length; i++){
 			
 			// mas, primeiro resetamos a lista de questões
-			answers = [];
+			questao = [];
 
 			// e aqui faremos para cada resposta na questão.
-			for(letter in questions[i].answers){
+			for(letter in questoes[i].questao){
 
 				// Aqui será escrito para html para usar o Radio.
-				answers.push(
+				questao.push(
 					'<label>'
-						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+						+ '<input type="radio" name="pergunta'+i+'" value="'+letter+'">'
 					//	+ letter + ') ' vou deixar a letra comentada, porque achei melhor assim
-						+ questions[i].answers[letter]
+						+ questoes[i].questao[letter]
 					+ '</label>'
 				);
 			}
 
 			// add this question and its answers to the output
 			output.push(
-				'<div class="question">' + questions[i].question + '</div>'
-				+ '<div class="answers">' + answers.join('') + '</div>'
+				'<div class="pergunta">' + questoes[i].pergunta + '</div>'
+				+ '<div class="questao">' + questao.join('') + '</div>'
 			);
 		}
 
@@ -144,46 +144,46 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 	}
 
 
-	function showResults(questions, quizContainer, resultsContainer){
+	function showResults(questoes, quizContainer, resultadoContainer){
 			
 		
-		var answerContainers = quizContainer.querySelectorAll('.answers');
+		var jsonContainers = quizContainer.querySelectorAll('.questao');
 		
 		
-		var userAnswer = '';
-		var numCorrect = 0;
+		var userJson = '';
+		var numCorreto = 0;
 		
 		
-		for(var i=0; i<questions.length; i++){
+		for(var i=0; i<questoes.length; i++){
 
 			// agora ele vai virar a letra que esta marcada.
-			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+			userJson = (jsonContainers[i].querySelector('input[name=pergunta'+i+']:checked')||{}).value;
 			
 			
 			
-			if(userAnswer===questions[i].correctAnswer){
+			if(userJson===questoes[i].respostaCorreta){
 				
-				numCorrect++;
+				numCorreto++;
 				
 				
-				answerContainers[i].style.color = 'lightgreen';
+				jsonContainers[i].style.color = 'lightgreen';
 			}
 			
 			else{
 				
-				answerContainers[i].style.color = 'red';
+				jsonContainers[i].style.color = 'red';
 			}
 		}
 
 			
-			resultsContainer.innerHTML = numCorrect + ' de ' + questions.length;
+			resultadoContainer.innerHTML = numCorreto + ' de ' + questoes.length;
 		}
 
 	// show questions right away
-	showQuestions(questions, quizContainer);
+	showQuestions(questoes, quizContainer);
 
 	// on submit, show results
 	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
+		showResults(questoes, quizContainer, resultadoContainer);
 	}
 }
